178a7ce8a4584aab354655d7a5aa25ff664372c2
Ravi Kumawat <49914487+Ravikumawatofficial@users.noreply.github.com>
2020-11-06 23:14:18 +0530

    Update incometax.c

--- incometax.c
+++ incometax.c
@@ -1,19 +1,25 @@
 #include<stdio.h>
 int main() {
 	float a;
+	float tax=0;
 	printf("Income Tax Calculator\n");
 	printf("Enter Your Total Yearly Income in INR\n");
 	scanf("%f", &a);
 	if(a<250000){
+		
 		printf("No Taxes Applicable.\n");
 	}
 	else if(a>=250000 && a<500000){
-		printf("Your Total Tax is %f", a*5/100);
+		tax=tax+0.05*(a-250000);
+		printf("Your Total Tax is %f", tax);
 	}
 	else if(a>=500000 && a<1000000){
-		printf("Your Total Tax is %f",a*20/100);
+		tax=tax+0.20*(a-500000);
+		printf("Your Total Tax is %f", tax);
 	}
-	else { printf("Your Total Tax is %f", a*30/100);
+	else {
+		tax=tax+0.30*(a-1000000);
+		 printf("Your Total Tax is %f", tax);
 		}
 		return 0;
 }
